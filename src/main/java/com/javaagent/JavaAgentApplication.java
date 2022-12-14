8742f55518c1064c8470621676f98651e15b0199
vanhieuhp <vanhieuit10@gmail.com>
2022-12-14 16:26:37 +0700

    init file

---
+++ src/main/java/com/javaagent/JavaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.javaagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaagent/JavaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
